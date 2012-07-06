c927ee30b29a9c3ef86ee24e3ba609f77e174925
Kevin McKinnis <kevinmckinnis928@gmail.com>
2012-07-05 19:12:20 -0700

    starting iRobo2013 project

---
+++ src/org/wintrisstech/teamhawk/irobot2013/Dashboard.java
@@ -0,0 +1,24 @@
+package org.wintrisstech.teamhawk.irobot2013;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class Dashboard extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_dashboard, menu);
+        return true;
+    }
+
+    
+}
