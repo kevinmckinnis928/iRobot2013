d928eeb254a644d402d7030024f70d9b29c8aa68
Kevin McKinnis <kevinmckinnis928@gmail.com>
2012-07-26 19:34:12 -0700

    creating new activity :)

--- src/org/wintrisstech/teamhawk/irobot2013/Dashboard.java
+++ src/org/wintrisstech/teamhawk/irobot2013/Dashboard.java
@@ -1,22 +1,37 @@
 package org.wintrisstech.teamhawk.irobot2013;
 
+import org.wintrisstech.irobot.ioio.R;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class Dashboard extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_dashboard, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_dashboard, menu);
+		return true;
+	}
+
+	public void sendMessage(View view) {
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+
+	}
+
 }
---
+++ src/org/wintrisstech/teamhawk/irobot2013/DisplayMessageActivity.java
@@ -0,0 +1,16 @@
+package org.wintrisstech.teamhawk.irobot2013;
+
+import org.wintrisstech.irobot.ioio.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_display_message);
+	}
+
+}
